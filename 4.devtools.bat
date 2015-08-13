40dac8e64019aaa5e67c5631e99c6c1ce63114b7
Stefan Prodan <stefan.prodan@gmail.com>
2015-08-13 13:58:44 +0300

    Chrome added

--- 4.devtools.bat
+++ 4.devtools.bat
@@ -2,4 +2,5 @@ choco install git.install -y
 choco install visualstudiocode -version 0.5.0.0 - y
 choco install visualstudio2015community -y
 choco install notepadplusplus.install -y
+choco install google-chrome-x64 -y
 choco install firefox -y
