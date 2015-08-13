31df93b25a19cecfa7a222570b541b26a53b3ee8
Stefan Prodan <stefan.prodan@gmail.com>
2015-08-13 13:45:58 +0300

    Update 4.devtools.bat
    VS.NET 2015 added

--- 4.devtools.bat
+++ 4.devtools.bat
@@ -1,4 +1,5 @@
 choco install git.install -y
 choco install visualstudiocode -version 0.5.0.0 - y
+choco install visualstudio2015community -y
 choco install notepadplusplus.install -y
 choco install firefox -y
