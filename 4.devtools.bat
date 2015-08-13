dba30a473dcc08922f3ba225c0023ab63ba3c10b
Stefan Prodan <stefan.prodan@gmail.com>
2015-08-13 14:01:31 +0300

    fiddler

--- 4.devtools.bat
+++ 4.devtools.bat
@@ -2,5 +2,6 @@ choco install git.install -y
 choco install visualstudiocode -version 0.5.0.0 - y
 choco install visualstudio2015community -y
 choco install notepadplusplus.install -y
+choco install fiddler -y
 choco install google-chrome-x64 -y
 choco install firefox -y
