57938f176787dedd08e954b9e4668ecaf645b7ac
Stefan Prodan <stefan.prodan@gmail.com>
2015-08-13 13:57:13 +0300

    Chocolatey GUI added

--- 1.choco.bat
+++ 1.choco.bat
@@ -1,2 +1,3 @@
 netsh advfirewall set  allprofiles state off
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+choco install chocolateygui
